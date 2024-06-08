711099fc6746826f58802c994283e419768bfd56
Trang131203 <ututcutehotme@gmail.com>
2024-06-08 12:09:37 +0700

    adjs

---
+++ resources/static/js/login.js
@@ -0,0 +1,9 @@
+function signup(){
+    document.querySelector("#loginForm").setAttribute("action", "/register");
+    document.querySelector("#loginForm").setAttribute("method", "get");
+    document.querySelector("#loginForm").submit();
+}
+
+function login() {
+    document.querySelector("#loginForm").submit();
+}
